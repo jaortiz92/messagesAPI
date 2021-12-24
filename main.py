6feee95e7dd81726c8abc9bc25fe98b9a099ee02
John Ortiz <jaortiz92@hotmail.com>
2021-12-24 18:55:59 -0500

    validations_path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 from models import Person
 
 app = FastAPI()
@@ -19,9 +19,38 @@ def create_person(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    email: str = Query(..., regex=".+@.+\..+"),
-    age: Optional[int] = Query(None, ge=18, le=100)
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person name",
+        description="This is the person name. It's between 1 and 50 characers."
+    ),
+    email: str = Query(
+        ...,
+        regex=".+@.+\..+",
+        title="Person email",
+        description="This is the email. It's required"
+    ),
+    age: Optional[int] = Query(
+        None,
+        ge=18,
+        le=100,
+        title="Person age",
+        description="This is the person ege, It's between 18 and 100 years"
+    )
 ):
     return {"email": email,
             name: age}
+
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person ID",
+        description="This is the person ID. It's greater than 0. It's required."
+    )
+):
+    return {person_id: "It exists!"}
