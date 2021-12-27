81428198a3bf7a39c96bcd34913005a5a06457c6
John Ortiz <jaortiz92@hotmail.com>
2021-12-26 20:39:31 -0500

    files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 from fastapi import status
 from pydantic.networks import EmailStr
 from pydantic.types import PositiveFloat
@@ -131,3 +131,16 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+
+@app.post(
+    path="/post-image",
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
